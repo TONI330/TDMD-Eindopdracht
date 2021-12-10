2599e866126a86518facb8b3c1559d94a9926756
Teun Leenders <t.j.tleenders@gmail.com>
2021-12-10 12:37:57 +0100

    added location code, doesn't work

--- app/src/main/java/com/example/sanic/GameActivity.kt
+++ app/src/main/java/com/example/sanic/GameActivity.kt
@@ -1,13 +1,24 @@
 package com.example.sanic
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import androidx.annotation.Nullable
+import androidx.core.app.ActivityCompat
 import com.example.sanic.databinding.ActivityGameBinding
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
@@ -20,9 +31,9 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(LayoutInflater.from(this))
         setContentView(R.layout.activity_game)
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         Configuration.getInstance().load(this, getSharedPreferences("osm_config", Context.MODE_PRIVATE))
-
+        getLastLocation()
         setupMaps()
     }
 
@@ -36,10 +47,53 @@ class GameActivity : AppCompatActivity() {
             overlays.add(locationOverlay)
 
             controller.apply {
-                
+//                val currentLocation = currentLocation()
+//                setCenter(currentLocation)
                 setZoom(6.0)
             }
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+            fusedLocationClient!!.lastLocation
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful && task.result != null) {
+                        val mLastLocation = task.result
+                        Log.d("debug", mLastLocation.toString())
+                    } else {
+                        Log.w("debug", "getLastLocation:exception", task.exception)
+                    }
+                }
+    }
+
+    @Nullable
+    private fun currentLocation() : GeoPoint? {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d("debug", "no permissions")
+
+            return null
+        }
+        Log.d("debug", "getting location..")
+        val token = CancellationTokenSource()
+            val currentLocation = fusedLocationClient?.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, token.token)
+            val locationPoint = currentLocation?.result?.let { GeoPoint(it.latitude, currentLocation.result.longitude) }
+        Log.d("debug", locationPoint.toString())
+        return locationPoint
     }
 
 }
