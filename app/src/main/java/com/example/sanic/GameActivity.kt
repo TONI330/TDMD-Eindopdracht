0c45280f20bf556ee0c9c63e1379ec8c0f206543
Teun Leenders <t.j.tleenders@gmail.com>
2021-12-10 14:32:32 +0100

    center is set to last location now

--- app/src/main/java/com/example/sanic/GameActivity.kt
+++ app/src/main/java/com/example/sanic/GameActivity.kt
@@ -14,8 +14,8 @@ import com.example.sanic.databinding.ActivityGameBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.CancellationToken
 import com.google.android.gms.tasks.CancellationTokenSource
+import org.osmdroid.api.IGeoPoint
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -30,15 +30,15 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(LayoutInflater.from(this))
-        setContentView(R.layout.activity_game)
+        setContentView(binding.root)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         Configuration.getInstance().load(this, getSharedPreferences("osm_config", Context.MODE_PRIVATE))
-        getLastLocation()
+        //getLastLocation()
         setupMaps()
     }
 
     private fun setupMaps() {
-
+        setCenterToLastLocation()
         binding.map.apply{
             setTileSource(TileSourceFactory.MAPNIK)
 
@@ -47,20 +47,24 @@ class GameActivity : AppCompatActivity() {
             overlays.add(locationOverlay)
 
             controller.apply {
-//                val currentLocation = currentLocation()
-//                setCenter(currentLocation)
-                setZoom(6.0)
+                Log.d("debug", "zooming")
+                setZoom(21.0)
             }
         }
     }
 
     @SuppressLint("MissingPermission")
-    private fun getLastLocation() {
-            fusedLocationClient!!.lastLocation
+    private fun setCenterToLastLocation() {
+        var locationPoint : IGeoPoint?
+        fusedLocationClient!!.lastLocation
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful && task.result != null) {
-                        val mLastLocation = task.result
-                        Log.d("debug", mLastLocation.toString())
+                        val lastLocation = task.result
+                        locationPoint = GeoPoint(lastLocation.latitude, lastLocation.longitude)
+                        Log.d("debug", "Last location: $locationPoint")
+                        binding.map.controller.apply {
+                            setCenter(locationPoint)
+                        }
                     } else {
                         Log.w("debug", "getLastLocation:exception", task.exception)
                     }
@@ -68,7 +72,7 @@ class GameActivity : AppCompatActivity() {
     }
 
     @Nullable
-    private fun currentLocation() : GeoPoint? {
+    private fun currentLocation() : IGeoPoint? {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -89,10 +93,16 @@ class GameActivity : AppCompatActivity() {
             return null
         }
         Log.d("debug", "getting location..")
+
         val token = CancellationTokenSource()
-            val currentLocation = fusedLocationClient?.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, token.token)
-            val locationPoint = currentLocation?.result?.let { GeoPoint(it.latitude, currentLocation.result.longitude) }
-        Log.d("debug", locationPoint.toString())
+        val locationPoint : IGeoPoint? = null
+        fusedLocationClient?.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, token.token)?.addOnSuccessListener { location ->
+            locationPoint.let { GeoPoint(location.latitude, location.longitude) }
+        }
+        //Log.d("debug", "Exception: " + currentLocation?.exception)
+
+        //val locationPoint = currentLocation?.result?.let { GeoPoint(it.latitude, currentLocation.result.longitude) }
+        Log.d("debug", "Current location: " + locationPoint.toString())
         return locationPoint
     }
 
